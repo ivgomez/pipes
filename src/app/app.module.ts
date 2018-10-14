901fa759e0108981acbfcbd1712968eadc83ffab
iv.gomez <iv.gomez@hotmail.com>
2018-10-14 10:08:00 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe'
+import  localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: "es" }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
